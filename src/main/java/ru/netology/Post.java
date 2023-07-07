5d8598b77ff4c27987eb5891999d588c254ab76a
Pain <vadimklimkin1@gmail.com>
2023-07-07 15:30:06 +0300

    еп

--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -1,4 +1,12 @@
-package org.example;
+package ru.netology;
 
 public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
 }
